1f9f19cf82738510fd0eb9c1f1d5c61cd14b9e2a
Minoru Sakamoto <p2020640@gmail.com>
2014-12-09 17:35:27 +0900

    under construction

--- src/main/scala/nazotoki/nazotoki.scala
+++ src/main/scala/nazotoki/nazotoki.scala
@@ -5,7 +5,7 @@ package nazotoki
  */
 object Nazotoki {
 
-  def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
+  def flatMapSublists[A, B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
     ls match {
       case Nil => Nil
       case sublist@(_ :: tail) => f(sublist) ::: flatMapSublists(tail)(f)
@@ -14,13 +14,21 @@ object Nazotoki {
   def combinations[A](n: Int, ls: List[A]): List[List[A]] =
     if (n == 0) List(Nil)
     else flatMapSublists(ls) { sl =>
-      combinations(n - 1, sl.tail) map {sl.head :: _}
+      combinations(n - 1, sl.tail) map {
+        sl.head :: _
+      }
     }
 
   def group[A](ns: List[Int], ls: List[A]): List[List[List[A]]] = ns match {
-    case Nil     => List(Nil)
+    case Nil => List(Nil)
     case n :: nss => combinations(n, ls) flatMap { c =>
-      group(nss, ls.filterNot(c.contains(_))) map {c :: _}
+      group(nss, ls.filterNot(c.contains(_))) map {
+        c :: _
+      }
     }
   }
+
+  def balance(ns: List[Int], ls: List[GamePlayer]): List[List[List[GamePlayer]]] = group(ns, ls).filter(_.exists(x => containMaster(x)))
+
+  def containMaster(ls: List[GamePlayer]) = ls.exists(_.isMaster)
 }
