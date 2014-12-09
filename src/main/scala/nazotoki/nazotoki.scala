c50eba0113899f34caa826a4d0e439e891745966
Minoru Sakamoto <p2020640@gmail.com>
2014-12-09 13:53:37 +0900

    change class to object

--- src/main/scala/nazotoki/nazotoki.scala
+++ src/main/scala/nazotoki/nazotoki.scala
@@ -1,9 +1,26 @@
+package nazotoki
+
 /**
  * grouping tool
  */
+object Nazotoki {
 
-package nazotoki
+  def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
+    ls match {
+      case Nil => Nil
+      case sublist@(_ :: tail) => f(sublist) ::: flatMapSublists(tail)(f)
+    }
 
-class Nazotoki {
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else flatMapSublists(ls) { sl =>
+      combinations(n - 1, sl.tail) map {sl.head :: _}
+    }
 
+  def group[A](ns: List[Int], ls: List[A]): List[List[List[A]]] = ns match {
+    case Nil     => List(Nil)
+    case n :: nss => combinations(n, ls) flatMap { c =>
+      group(nss, ls.filterNot(c.contains(_))) map {c :: _}
+    }
+  }
 }
