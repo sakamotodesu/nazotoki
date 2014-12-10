62686b3080627fcd9df0854af1d5fa94f4a46001
Minoru Sakamoto <p2020640@gmail.com>
2014-12-10 10:54:19 +0900

    under construction

--- src/main/scala/nazotoki/nazotoki.scala
+++ src/main/scala/nazotoki/nazotoki.scala
@@ -28,10 +28,25 @@ object Nazotoki {
     }
   }
 
-  def balance(ns: List[Int], ls: List[GamePlayer]): List[List[List[GamePlayer]]] = group(ns, ls).filter(_.forall(x => containMaster(x))).filter(_.forall(x => mixGender(x)))
+  def balance(ns: List[Int], ls: List[GamePlayer]): List[List[List[GamePlayer]]] = group(ns, ls)
+          .filter(_.forall(x => containMaster(x)))
+          .filter(_.forall(x => mixGender(x)))
+          .filter(_.forall(x => friends(x)))
+          .filter(_.forall(x => pair(x, "shutyou", "yukari")))
+          .filter(_.forall(x => pair(x, "nisshiy", "satoko")))
+          .map(x => x.map(y => y.sortWith((a, b) => a.name > b.name))
+          .sortWith((a, b) => a(0).name > b(0).name))
+          .distinct
+
 
   def containMaster(ls: List[GamePlayer]) = ls.exists(_.isMaster)
 
   def mixGender(ls: List[GamePlayer]) = ls.exists(_.isMale) && ls.exists(!_.isMale)
 
+  def friends(ls: List[GamePlayer]) = ls.forall(x => x.friends.exists(s => ls.exists(z => z.name == s)))
+
+  def pair(ls: List[GamePlayer], boy: String, girl: String) =
+    if (ls.exists(_.name == boy)) ls.exists(_.name == girl)
+    else if (ls.exists(_.name == girl)) ls.exists(_.name == boy)
+    else true
 }
