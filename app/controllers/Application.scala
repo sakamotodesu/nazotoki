37486f60ba229e3af0b6e094baf09af5a1e193bb
Minoru Sakamoto <p2020640@gmail.com>
2014-12-15 16:38:15 +0900

    add user select

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,9 @@
 package controllers
 
-import controllers.nazotoki.GamePlayer
 import controllers.nazotoki.Nazotoki._
 import controllers.nazotoki.Players._
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
 object Application extends Controller {
@@ -10,8 +11,31 @@ object Application extends Controller {
   def index = Action {
     //Ok(views.html.index("Your new application is ready!!!!!!!!"))
     //Ok(balance(List(4, 4, 5), players, friends).toString())
-    Ok(views.html.index(balance( players, friends)))
+    Ok(views.html.index(balance(players, friends)))
   }
 
-  def list(todayPlayers:List[Any]) = TODO
+  def list = Action {
+    Ok(views.html.list(test01Form, players))
+  }
+
+  def result = Action { implicit request =>
+    val (sakamotoBoolean, ataruBoolean, yukkiyBoolean, nisshiyBoolean, kamiyaBoolean, yukariBoolean, mayumiBoolean, ayumiBoolean, masudaBoolean, rodionBoolean, satokoBoolean, shutyouBoolean, monzenBoolean) = test01Form.bindFromRequest.get
+    Ok(sakamotoBoolean.toString+ataruBoolean.toString+yukkiyBoolean.toString)
+  }
+
+  val test01Form = Form(
+    tuple(sakamoto.name -> boolean,
+      ataru.name -> boolean,
+      yukkiy.name -> boolean,
+      nisshiy.name -> boolean,
+      kamiya.name -> boolean,
+      yukari.name -> boolean,
+      mayumi.name -> boolean,
+      ayumi.name -> boolean,
+      masuda.name -> boolean,
+      rodion.name -> boolean,
+      satoko.name -> boolean,
+      shutyou.name -> boolean,
+      monzen.name -> boolean)
+  )
 }
